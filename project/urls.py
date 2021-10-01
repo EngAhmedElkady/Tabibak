fcdff2d52edf9f39d4006354a016c0cfb3a06e7d
EngAhmedElkady <ahelkady110@gmail.com>
2021-10-02 00:09:40 +0200

    update project/urls.py

--- project/urls.py
+++ project/urls.py
@@ -18,7 +18,7 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from djange.views.static import serve
+from django.views.static import serve
 from django.conf.urls import url
 
 
@@ -30,7 +30,6 @@ urlpatterns = [
     path('Diabetes/', include('Diabetes.urls')),
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
-
 
 ]
 
