3c4e37b57c221e28c02d17f31280681c1497f137
EngAhmedElkady <ahelkady110@gmail.com>
2022-02-14 17:42:50 +0200

    add token for login

--- account/views.py
+++ account/views.py
@@ -13,6 +13,8 @@ from rest_framework.response import Response
 from .serializers import *
 
 # Register API
+
+
 class RegisterAPI(generics.GenericAPIView):
     serializer_class = RegisterSerializer
 
@@ -20,13 +22,12 @@ class RegisterAPI(generics.GenericAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
-        p=Profile(user=user)
+        p = Profile(user=user)
         p.save()
         return Response({
-        "user": UserSerializer(user, context=self.get_serializer_context()).data,
-        "token": AuthToken.objects.create(user)[1]
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            "token": AuthToken.objects.create(user)[1]
         })
-
 
 
 class LoginAPI(KnoxLoginView):
@@ -40,40 +41,39 @@ class LoginAPI(KnoxLoginView):
         super(LoginAPI, self).post(request, format=None)
         return Response(
             {
-                "user":user.id
+                "user": user.id,
+                "token": AuthToken.objects.create(user)[1]
+
 
             }
-        ) 
-    
+        )
 
 
-@api_view(['GET','PUT'])
+@api_view(['GET', 'PUT'])
 def profile_data(request):
 
     if request.method == 'PUT':
-        user1=request.user
-        profile=Profile.objects.get(user=user1)
-        profile.phone=request.data['phone']
-        profile.photo=request.data['photo']
+        user1 = request.user
+        profile = Profile.objects.get(user=user1)
+        profile.phone = request.data['phone']
+        profile.photo = request.data['photo']
         profile.save()
         return Response({
-            "phone":profile.phone,
-            "photo":str(profile.photo),
+            "phone": profile.phone,
+            "photo": str(profile.photo),
         })
 
     else:
-        user1=request.user
-        profile=Profile.objects.get(user=user1)
-        data1={}
-        data1["username"]=user1.username
-        data1["email"]=user1.email
-        data1["phone"]=profile.phone
-        data1["photo"]=profile.photo
+        user1 = request.user
+        profile = Profile.objects.get(user=user1)
+        data1 = {}
+        data1["username"] = user1.username
+        data1["email"] = user1.email
+        data1["phone"] = profile.phone
+        data1["photo"] = profile.photo
         return Response({
-            "username":user1.username,
-            "email":user1.email,
-            "phone":profile.phone,
-            "photo":str(profile.photo),
+            "username": user1.username,
+            "email": user1.email,
+            "phone": profile.phone,
+            "photo": str(profile.photo),
         })
-
-
